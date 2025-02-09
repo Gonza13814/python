427439bb2cef1fd7663e607265a87430ca541147
Lucio <luciocienfuegos41@gmail.com>
2025-02-09 16:34:12 -0300

    ejercicio 8 funcion esPrimo

--- guia4/8.py
+++ guia4/8.py
@@ -1,12 +1,12 @@
-"""8) esPrimo, que devuelva si un número entero dado como parámetro es o no primo. """
+"""8 esPrimo, que devuelva si un número entero dado como parámetro es o no primo. """
 
 def esPrimo(numero):
-    return not (numero % 2 == 0 and numero % numero == 0) or (numero % 3 == 0 and numero % numero == 0) or (numero % 5 == 0 and numero % numero == 0) or (numero % 7 == 0 and numero % numero == 0)
+    divisoresDelNumero = 0
+    for i in range(1, numero + 1):
+        if numero % i == 0:
+            divisoresDelNumero += 1
+    
+    return divisoresDelNumero == 2
 
- 
+print(esPrimo(13))
 
-print(esPrimo(6))
-
-print("soy gay")
-
-print("ES GAY")
